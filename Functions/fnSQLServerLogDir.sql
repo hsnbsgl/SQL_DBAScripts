ca6e7b14e45bf70e726b178bca4622eceb63aa06
hsnbsgl <34284320+hsnbsgl@users.noreply.github.com>
2019-02-28 10:00:45 +0300

    Create fnSQLServerLogDir.sql

---
+++ Functions/fnSQLServerLogDir.sql
@@ -0,0 +1,22 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER FUNCTION [dbo].[fnSQLServerLogDir]()
+RETURNS NVARCHAR(4000)
+AS
+BEGIN 
+
+	DECLARE @rc INT,@dir NVARCHAR(4000) ;
+
+	EXEC @rc = master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',N'Software\Microsoft\MSSQLServer\MSSQLServer',N'DefaultLog', @dir OUTPUT, 'no_output' ;
+
+	IF (@dir IS NULL)
+	BEGIN
+		EXEC @rc = master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',N'Software\Microsoft\MSSQLServer\Setup',N'SQLDataRoot', @dir OUTPUT, 'no_output';
+		SELECT @dir = @dir + N'\Data';
+	END 
+
+	RETURN @dir ;
+
+END
