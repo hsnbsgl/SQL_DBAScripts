219636f508c467182dbed963785d3d106c8cb2dd
hsnbsgl <34284320+hsnbsgl@users.noreply.github.com>
2019-02-28 09:02:56 +0300

    Create spReport_QueryStore_TopNQueriesbyAverageLogicalIOInLastNHours.sql

---
+++ Reports/spReport_QueryStore_TopNQueriesbyAverageLogicalIOInLastNHours.sql
@@ -0,0 +1,92 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE OR ALTER PROCEDURE [dbo].[spReport_QueryStore_TopNQueriesbyAverageLogicalIOInLastNHours] 
+@argDBName VARCHAR(128) = NULL, @argTop_N SMALLINT,@argInLastNHours SMALLINT
+AS
+BEGIN
+
+	SET NOCOUNT ON ;
+
+	IF @argTop_N IS NULL
+		SELECT @argTop_N = 100;
+
+	IF @argInLastNHours IS NULL
+		SELECT @argInLastNHours = 8;
+
+		
+	DROP TABLE IF EXISTS #Temp_Result ;
+
+    CREATE TABLE #Temp_Result
+        (
+          [DB_Name] VARCHAR(128) NULL ,
+          [avg_logical_io_reads] BIGINT ,
+          [Query_text_id] BIGINT ,
+		  [Query_id] BIGINT ,
+          [Query_sql_text] NVARCHAR(MAX) ,
+		  [Object_Name] VARCHAR(256) ,
+		  [Plan_id] BIGINT ,
+          [LocalLastExecutionTime] VARCHAR(50) 
+        );
+	
+	DECLARE @lcSQL NVARCHAR(MAX), @lcDB_Name VARCHAR(100);
+
+	DECLARE Database_Cursor CURSOR LOCAL FAST_FORWARD READ_ONLY FOR 
+	SELECT name FROM sys.databases WITH (NOLOCK)
+	WHERE name = @argDBName OR @argDBName IS NULL
+
+	OPEN Database_Cursor
+	FETCH NEXT FROM Database_Cursor INTO @lcDB_Name
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+        SELECT  @lcSQL = 'USE ' + QUOTENAME(@lcDB_Name) + ';									
+						SELECT TOP (@argTop_N)
+								DB_NAME(),
+								[rs].[avg_logical_io_reads] ,
+								[qst].[query_text_id] ,
+								[qsq].[query_id] ,
+								[qst].[query_sql_text] ,
+								CASE WHEN [qsq].[object_id] = 0 THEN N''Ad-hoc''
+									 ELSE OBJECT_NAME([qsq].[object_id])
+								END AS [ObjectName] ,
+								[qsp].[plan_id] ,
+								( DATEADD(MINUTE, -( DATEDIFF(MINUTE, GETDATE(), GETUTCDATE()) ),
+										  [rs].[last_execution_time]) ) AS [LocalLastExecutionTime]
+						FROM    [sys].[query_store_query] [qsq]
+								JOIN [sys].[query_store_query_text] [qst] ON [qsq].[query_text_id] = [qst].[query_text_id]
+								JOIN [sys].[query_store_plan] [qsp] ON [qsq].[query_id] = [qsp].[query_id]
+								JOIN [sys].[query_store_runtime_stats] [rs] ON [qsp].[plan_id] = [rs].[plan_id]
+						WHERE   [rs].[last_execution_time] > DATEADD(HOUR, -@argInLastNHours, GETUTCDATE())
+						ORDER BY [rs].[avg_logical_io_reads] DESC; 
+						';
+		
+		--PRINT @lcSQL
+		INSERT INTO #Temp_Result
+		EXEC sp_executesql @lcSQL, N'@argTop_N SMALLINT, @argInLastNHours SMALLINT', @argTop_N , @argInLastNHours 
+				
+   		FETCH NEXT FROM Database_Cursor INTO @lcDB_Name
+	END
+	CLOSE Database_Cursor 
+	DEALLOCATE Database_Cursor
+
+    SELECT TOP ( @argTop_N )
+            [DB_Name] ,
+            [avg_logical_io_reads] ,
+            [Object_Name] ,
+            [Query_text_id] ,
+            [Query_id] ,
+            [Plan_id] ,
+            [Query_sql_text] ,
+            [LocalLastExecutionTime]
+    FROM    #Temp_Result
+    ORDER BY [avg_logical_io_reads] DESC;
+
+	DROP TABLE #Temp_Result;
+	
+END
+
+
+
+
