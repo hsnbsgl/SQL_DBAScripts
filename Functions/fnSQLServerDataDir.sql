a9314595d8d60bd17148d82c7d462f209dd9178a
hsnbsgl <34284320+hsnbsgl@users.noreply.github.com>
2019-02-28 09:57:14 +0300

    Create fnSQLServerDataDir.sql

---
+++ Functions/fnSQLServerDataDir.sql
@@ -0,0 +1,24 @@
+ 
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE OR ALTER FUNCTION [dbo].[fnSQLServerDataDir]()
+RETURNS NVARCHAR(4000)
+AS
+BEGIN 
+
+	DECLARE @rc INT,@dir NVARCHAR(4000);
+
+	EXEC @rc = master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',N'Software\Microsoft\MSSQLServer\MSSQLServer',N'DefaultData', @dir OUTPUT, 'no_output' 
+
+	IF (@dir is null)
+	BEGIN
+		EXEC @rc = master.dbo.xp_instance_regread N'HKEY_LOCAL_MACHINE',N'Software\Microsoft\MSSQLServer\Setup',N'SQLDataRoot', @dir OUTPUT, 'no_output'
+		SELECT @dir = @dir + N'\Data'
+	END 
+
+	RETURN @dir 
+
+END
+
